acbec3c2871551445313b45c0e43e691be74b0a1
Marc S. Brooks <marc@mbrooks.info>
2014-11-28 13:58:00 -0800

    Updated code format - aligned JSON/Object delimiters left
    Resolved several JSHint warnings

--- yalb.js
+++ yalb.js
@@ -16,16 +16,21 @@ if (!window.jQuery || (window.jQuery && window.jQuery.fn.jquery < '1.8.3')) {
 
 (function($) {
 	var methods = {
-		"init" : function(options) {
+		"init": function(options) {
 
 			// default options
 			var settings = $.extend({
-				maskColor  : '#000000',
-				showEasing : 'linear',
-				hideEasing : 'linear',
-				hideSpeed  : 250,
-				showSpeed  : 500
+				maskColor:  '#000000',
+				showEasing: 'linear',
+				hideEasing: 'linear',
+				hideSpeed:  250,
+				showSpeed:  500
 			}, options);
+
+			// force window height in IE
+			if ($.browser.msie) {
+				$('body').height('100%');
+			}
 
 			return this.each(function() {
 				var $this = $(this),
@@ -33,41 +38,36 @@ if (!window.jQuery || (window.jQuery && window.jQuery.fn.jquery < '1.8.3')) {
 
 				// remove vertical margins
 				$('html, body').css({
-					marginBottom : 0,
-					marginTop    : 0
+					marginBottom: 0,
+					marginTop:    0
 				});
 
 				if ( $.isEmptyObject(data) ) {
 					var win = createWindow($this, settings);
 
 					$this.data({
-						modal   : win.children('div.modal'),
-						mask    : win.children('div.mask'),
-						options : settings
+						modal:   win.children('div.modal'),
+						mask:    win.children('div.mask'),
+						options: settings
 					});
 
 					$('body').append(win);
 				}
 			});
-
-			// force window height in IE
-			if ($.browser.msie) {
-				$('body').height('100%');
-			}
 		},
 
-		"destroy" : function() {
+		"destroy": function() {
 			return this.each(function() {
 				$(this).removeData();
 			});
 		},
 
-		"show" : function(callback) {
+		"show": function(callback) {
 			return this.each(function() {
 				var $this = $(this),
 					data  = $this.data();
 
-				var attr = { cursor : 'pointer' };
+				var attr = { cursor: 'pointer' };
 
 				data.modal
 					.fadeTo(data.options.showSpeed, 1, data.options.showEasing,
@@ -93,15 +93,15 @@ if (!window.jQuery || (window.jQuery && window.jQuery.fn.jquery < '1.8.3')) {
 			});
 		},
 
-		"hide" : function(callback) {
+		"hide": function(callback) {
 			return this.each(function() {
 				var $this = $(this),
 					data  = $this.data();
 
 				var attr = {
-					cursor  : 'auto',
-					display : 'none',
-					zIndex  : 0
+					cursor:  'auto',
+					display: 'none',
+					zIndex:  0
 				};
 
 				data.modal
@@ -148,11 +148,11 @@ if (!window.jQuery || (window.jQuery && window.jQuery.fn.jquery < '1.8.3')) {
 			= $('<div></div>')
 				.addClass('modal')
 				.css({
-					backgroundColor : 'transparent',
-					display         : 'none',
-					height          : node.outerHeight(),
-					width           : node.outerWidth(),
-					opacity         : 0
+					backgroundColor: 'transparent',
+					display: 'none',
+					height:  node.outerHeight(),
+					width:   node.outerWidth(),
+					opacity: 0
 				})
 				.append(node);
 
@@ -160,11 +160,11 @@ if (!window.jQuery || (window.jQuery && window.jQuery.fn.jquery < '1.8.3')) {
 			= $('<div></div>')
 				.addClass('mask')
 				.css({
-					backgroundColor : options.maskColor,
-					display         : 'none',
-					height          : '100%',
-					width           : '100%',
-					opacity         : 0
+					backgroundColor: options.maskColor,
+					display: 'none',
+					height:  '100%',
+					width:   '100%',
+					opacity: 0
 				});
 
 		return $('<div></div>')
@@ -181,14 +181,14 @@ if (!window.jQuery || (window.jQuery && window.jQuery.fn.jquery < '1.8.3')) {
 			posY = getBrowserCenterY() - (node.outerHeight() / 2);
 
 		node.css({
-			position : 'absolute',
-			left     : (posX > 0) ? posX : 0,
-			top      : (posY > 0) ? posY : 0,
-			zIndex   : 150
+			position: 'absolute',
+			left:     (posX > 0) ? posX : 0,
+			top:      (posY > 0) ? posY : 0,
+			zIndex:   150
 		});
 
 		// update on browser resize event
-		window.onresize = function() { setWindowProps(node) };
+		window.onresize = function() { setWindowProps(node); };
 	}
 
 	/**
@@ -197,11 +197,11 @@ if (!window.jQuery || (window.jQuery && window.jQuery.fn.jquery < '1.8.3')) {
 	 */
 	function setMaskProps(node) {
 		node.css({
-			position : 'absolute',
-			top      : 0,
-			left     : 0,
-			height   : getDocHeight(),
-			zIndex   : 100
+			position: 'absolute',
+			top:      0,
+			left:     0,
+			height:   getDocHeight(),
+			zIndex:   100
 		});
 	}
 
